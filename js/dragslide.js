84008f4b7081e3ddfe2dfc73979e7a232f9d8c8a
taeyeess <taeyeeya@gmail.com>
2022-07-04 17:38:19 +0900

    드래그슬라이드 해결못함..

--- js/dragslide.js
+++ js/dragslide.js
@@ -1,17 +1,43 @@
-var sliderrange = document.getElementById("myRange");
-var output = document.getElementById("value");
-var list = document.querySelectorAll('.list');
+var slidecontainer = document.querySelector('.slidecontainer');
+ var myRange = document.getElementById("myRange");
+ var list_1 = document.getElementsByClassName("list1");
+ var list_2 = document.getElementsByClassName("list2");
+ var list_3 = document.getElementsByClassName("list3");
+ var list_4 = document.getElementsByClassName("list4");
 
-output.innerHTML = sliderrange.value;
+ // output.innerHTML = slider.value; // 초기값 0
 
-sliderrange.oninput = function () {
-    for(let i=0; i < list.length; i++){
-        console.log(list[i]);        
+
+myRange.oninput = function () {
+    //console.log(this.value);
+    // if(this.value < 2){
+    //     list_1.style.left = '0px';        
+    // }else if(this.value >= 2 && this.value < 3){
+    //     list_2.style.left = '0px';
+    // }else if(this.value >= 3 && this.value < 4){
+    //     list_3.style.left = '0px';
+    // }else if(this.value === 4){
+    //     list_4.style.left = '0px';
+    // }
        
-    }    
-    output.innerHTML = this.value;   
-    
+    // output.innerHTML = this.value;    
 }
+
+myRange.addEventListener('input', updateValue);
+
+function updateValue(e) {
+    if(e.target.value < 2){
+        list_1.style.left = '0px';
+    }else if(e.target.value >= 2 && e.target.value < 3){
+        list_2.style.left = '0px';
+    }else if(e.target.value >= 3 && e.target.value < 4){
+        list_3.style.left = '0px';
+    }else if(e.target.value === 4){
+        list_4.style.left = '0px';
+    }
+    
+  }
+
  // if(this.value < 25){
         //     list[i].style.left('0px');        
         // }else if(25 < this.value < 50){
@@ -21,3 +47,5 @@ sliderrange.oninput = function () {
         // }else if(75 < this.value < 90){
         //     list[3].style.left('0px');
         // }
+
+
