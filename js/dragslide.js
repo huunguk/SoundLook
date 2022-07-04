98e0d272ea36b5fcd027b83a044245b20d3af68e
taeyeess <taeyeeya@gmail.com>
2022-07-04 14:01:45 +0900

    dragslide.. 코드1차 작성

--- js/dragslide.js
+++ js/dragslide.js
@@ -1,7 +1,23 @@
 var slider = document.getElementById("myRange");
 var output = document.getElementById("value");
+var list = document.querySelectorAll('.list');
+
 output.innerHTML = slider.value;
 
 slider.oninput = function () {
-    output.innerHTML = this.value;
+    for(let i=0; i < list.length; i++){
+        console.log(list[i]);        
+       
+    }    
+    output.innerHTML = this.value;   
+    
 }
+ // if(this.value < 25){
+        //     list[i].style.left('0px');        
+        // }else if(25 < this.value < 50){
+        //     list[i].style.left('0px');
+        // }else if(50 < this.value < 75){
+        //     list[2].style.left('0px');
+        // }else if(75 < this.value < 90){
+        //     list[3].style.left('0px');
+        // }
