9b26dfb71299d618732adae7454ee8f3b90c6c4c
huunguk <huunguk@gmail.com>
2022-07-04 10:50:43 +0900

    The slidecontainer was fixed

--- js/dragslide.js
+++ js/dragslide.js
@@ -2,6 +2,6 @@ var slider = document.getElementById("myRange");
 var output = document.getElementById("value");
 output.innerHTML = slider.value;
 
-slider.oninput = function() {
+slider.oninput = function () {
     output.innerHTML = this.value;
 }
