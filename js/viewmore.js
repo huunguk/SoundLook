04cea30bc939e55bbb42c2ff3e85662c11c07916
hajin <ahj.hajin@gmail.com>
2022-07-06 10:10:16 +0900

    product javascript update

---
+++ js/viewmore.js
@@ -0,0 +1,16 @@
+let img = document.querySelectorAll('.product_img');
+let view = document.querySelector('.view');
+let currentImg = 3;
+
+view.addEventListener('click',function(){
+    //console.log('check')
+    for(let i = currentImg; i < currentImg + 3; i++){
+        if(img[i]){
+             img[i].style.display = 'block';
+        }
+    }
+    currentImg += 3;
+    if(currentImg >= img.length){
+        event.target.style.display = 'none';
+    }
+});
