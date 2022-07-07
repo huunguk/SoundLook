4f145121a2682bf84add19de3086d8781dca1f95
hajin <ahj.hajin@gmail.com>
2022-07-07 10:17:21 +0900

    스크립트 연결

--- js/viewmore.js
+++ js/viewmore.js
@@ -2,7 +2,7 @@ let img = document.querySelectorAll('.product_img');
 let view = document.querySelector('.view');
 let currentImg = 3;
 
-view.addEventListener('click',function(){
+view.addEventListener('click',function(event){
     //console.log('check')
     for(let i = currentImg; i < currentImg + 3; i++){
         if(img[i]){
