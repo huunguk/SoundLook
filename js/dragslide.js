4e3a2d53881b23602fab3b2b50d6027e34366805
taeyee <taeyeeya@gmail.com>
2022-07-16 19:32:49 +0900

    레이아웃 조절

--- js/dragslide.js
+++ js/dragslide.js
@@ -4,7 +4,7 @@ const slideContainerUl = document.querySelector(".slidecontainer ul");
 function handleSlide(e) {
   const value = e.target.value;
   console.log(value);
-  slideContainerUl.style.marginLeft = `${value * -96}px`;
+  slideContainerUl.style.marginLeft = `${value * -80}px`;
 }
 
 function init () {
